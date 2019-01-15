7079a61a4adf49d60e175fa9e3e07ace16defb46
Emilio <emilioplatzer@gmail.com>
2019-01-15 10:49:09 -0300

    prepare graba el archivo

--- script/prepare-inst.sh
+++ script/prepare-inst.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echo Preparando instalación v0.1.0
+coderun_version = "0.1.1"
+echo "Preparando instalación $coderun_version"
 read -p 'Nombre de instancia:' inst_name
+file_name="/opt/insts/$inst_name.yaml"
 if [[ -z "$inst_name" ]]; then
   echo "debe poner el nombre de instancia"
+elif [[ -f "$file_name" ]]; then
+  echo "ya existe el archivo $file_name"
+  echo "no se puede crear la instancia"
 else
 
   echo "# server section"
@@ -27,17 +32,17 @@ else
   db_name=${db_name:-${inst_name}_db}
   read -p "username [${inst_name}_admin]:" db_user
   db_user=${db_user:-${inst_name}_admin}
+  read -p "owner [${inst_name}_owner]:" db_owner
+  db_owner=${db_user:-${inst_name}_owner}
   db_pass=''
   while [[ -z "$db_pass" ]]
     do
       read -p "password:" db_pass
     done
-  read -p "owner [${inst_name}_owner]:" db_owner
-  db_owner=${db_user:-${inst_name}_owner}
-  echo "el owner va sin password"
 
-  echo "por instalar $inst_name en $port sobre /$base_url desde https://$git_host.com/$git_group/$inst_name"
-  echo "db: $db_name user for connect: $db_user owner: $db_owner"
+
+  echo "por instalar $inst_name en $port sobre /$base_url desde https://$git_host.com/$git_group/$git_project"
+  echo "db: $db_name usuario para conexion: $db_user owner: $db_owner"
 
   ok=''
   while [[ -z "$ok" ]]
@@ -48,5 +53,25 @@ else
      echo "abortado"
   else
     echo "preparando"
+    cat >$file_name <<YAML
+coderun:
+  version: $coderun_version
+server:
+  port: $port
+  base-url: /$base_url
+  session-store: memory
+git:
+  project: $git_project
+  group: $git_group
+  host: https://$git_host.com
+db:
+  database: $db_name
+  user: $db_user
+  password: $db_pass
+install:
+  dump:
+    db:
+      owner: $db_owner
+YAML
   fi
 fi
