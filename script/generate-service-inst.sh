cf35003a81c7167e6b2800e442d3dd19271a5ea6
Emilio <emilioplatzer@gmail.com>
2019-01-15 12:21:18 -0300

    generate-service-inst.sh

---
+++ script/generate-service-inst.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+coderun_version="0.1.1"
+echo "Generacion servicio $coderun_version"
+file_name="/opt/services/${nombre_dir}.service"
+cp $file_name ~/copia_${nombre_dir}
+    cat >$file_name <<SERVICE
+[Unit]
+Description=$nombre_dir - node
+[Service]
+ExecStart=/opt/bin/coderun/script/run-app.sh
+Restart=always
+RestartSec=5
+WorkingDirectory=/opt/npm/$nombre_dir
+User=$nombre_dir
+Group=runner
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=$nombre_dir
+[Install]
+WantedBy=multi-user.target
+SERVICE
+diff $file_name ~/copia_${nombre_dir}
