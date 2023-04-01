59a61a4f3e0897c581b70505f6410aa482f4733f
Emilio Platzer <emilioplatzer@gmail.com>
2023-04-01 11:51:59 -0300

    oculto la clave en la pantalla en el ejemplo de local-path

--- devel/example-local-path.bat
+++ devel/example-local-path.bat
@@ -1,6 +1,7 @@
 set PGPORT=54313
 set PGUSER=postgres
-set PGPASSWORD=cambiar_esta_clave_ahora
+echo set PGPASSWORD=********
+@set PGPASSWORD=cambiar_esta_clave_ahora
 rem Quitar el rem de la próxima si no está el psql en el path (y poner el path correcto, en especial la versión de PG):
 rem set "Path=%path%;c:\Program Files\PostgreSQL\15\bin\"
 rem Quitar el rem de la próxima si se queire el autorun (verificar path)
