ee7a25076a4180bc6630d9d786246fe16fd4edcf
Eugenio Arosteguy <eugenio.arosteguy@gmail.com>
2024-10-24 15:16:02 -0300

    se cambia pg por pg-promise-strict

--- devel/backups_externos/get_databases.js
+++ devel/backups_externos/get_databases.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { Client } = require('pg');
+const pg = require('pg-promise-strict');
 const fs = require('fs');
 const yaml = require('js-yaml');
 
@@ -13,17 +13,17 @@ function loadConfig() {
         process.exit(1);
     }
 }
-
 // Obtener configuración
 const localConfig = loadConfig();
-const instrumentacionDBClient = new Client({
+const conOpts = {
     host: localConfig.db_instrumentacion.host,
     user: localConfig.db_instrumentacion.user,
     port: localConfig.db_instrumentacion.port,
     database: localConfig.db_instrumentacion.database,
     options: `--search_path=${localConfig.db_instrumentacion.schema}`,
     password: localConfig.db_instrumentacion.password
-});
+};
+var client=undefined;
 
 async function getEnginesToBackup() {
     try {
@@ -33,7 +33,7 @@ async function getEnginesToBackup() {
             where m.producto ='postgres' and s.usuario_backups_externos = $1
             order by s.ip, m.puerto;
         `;
-        const res = await instrumentacionDBClient.query(query, [localConfig.usuario_inst_responsable_backup]);
+        const res = await client.query(query, [localConfig.usuario_inst_responsable_backup]).fetchAll();
         return res.rows;
     } catch (err) {
         console.error('Error al obtener servidores:', err);
@@ -43,13 +43,17 @@ async function getEnginesToBackup() {
 
 async function getDatabases(engine) {
     try {
-        const res = await instrumentacionDBClient.query(
+        
+        const res = await client.query(
             `SELECT database, s.ip, db.port 
             FROM instrumentacion.servidores s 
             left join instrumentacion.databases db using (servidor)
             where s.ip = $1 and db.port = $2 
-            AND db.database !~ 'muleto|template|postgres|bkp|bak|capa';
-        `,[engine.host, engine.puerto]);
+            AND db.database !~ 'muleto|template|postgres|bkp|bak|capa|old|hasta';
+        `,[engine.host, engine.puerto]).fetchAll();
+        // ###################################
+        // TODO VOLVER A AGREGAR AL PATRON test|prueba|
+        // ###################################
         return res.rows.map(row => row.database);
     } catch (err) {
         console.error('Error al obtener servidores:', err);
@@ -59,7 +63,7 @@ async function getDatabases(engine) {
 
 async function writeDatabasesToFile() {
     try {
-        await instrumentacionDBClient.connect();
+        client = await pg.connect(conOpts)
         const engines = await getEnginesToBackup();
         const backupListPath = './local-databases_to_backup.txt';
         const writeStream = fs.createWriteStream(backupListPath);
@@ -71,7 +75,7 @@ async function writeDatabasesToFile() {
             }
         }
         writeStream.end();
-        await instrumentacionDBClient.end();
+        client.done();
 
         console.log(`Lista de bases de datos guardada en ${backupListPath}`);
     } catch (err) {
